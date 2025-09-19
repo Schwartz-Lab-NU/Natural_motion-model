025436672985211345b5ca377297873d6202d580
komeijiii <komeijiiiii@gmail.com>
2025-09-19 16:41:34 -0500

    new version of V3 091825B

---
+++ .history/V3/opt_batch/V3_control_counts_gaussian_20250919161410.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --account=p32750  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=24 ## how many cpus or processors do you need on each computer
+#SBATCH --time=04:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=8G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=V3_control_counts_gaussian  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=V3_control_counts_gaussian ## standard out and standard error goes to this file
+
+module purge all
+module load python-miniconda3
+eval "$(conda shell.bash hook)"
+conda activate /home/yuj1135/.conda/envs/elephant
+
+# Set working directory
+cd /projects/p32750/repository/Natural_motion-model/V3/opt_batch
+
+
+
+# Print job information
+echo "Starting V3 Control counts Gaussian Optimization"
+echo "Job ID: $SLURM_JOB_ID"
+echo "Node: $SLURM_NODELIST"
+echo "Working directory: $(pwd)"
+echo "Date: $(date)"
+
+# Run the optimization  Gaussian
+python V3_control_counts_gaussian.py
+
+# Check exit status
+if [ $? -eq 0 ]; then
+    echo "V3 Control counts Gaussian optimization completed successfully"
+    echo "Results saved to: /projects/p32750/repository/Natural_motion-model/V3/results/V3_control_counts_gaussian.pkl"
+else
+    echo "V3 Control counts Gaussian optimization failed with exit code $?"
+    exit 1
+fi
+
+echo "Job completed at: $(date)" 
